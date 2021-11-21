561534bbec7a3da7bd0d8abf8b93b340052f3c70
Jeffry Azhari Rosman <jeffryazhari@gmail.com>
2021-11-22 01:31:55 +0700

    update

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
     origin: "*",
-    allowedHeaders: ["Content-Type"],
   })
 );
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
